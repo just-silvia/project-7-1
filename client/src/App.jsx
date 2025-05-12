79fa42ec0400e241347bf565e977d24158e0b6cd
Alessia Sperindio <4l3ss14l4voro@gmail.com>
2025-05-12 12:48:21 +0200

    tofix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,15 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Public from "./layout/Public";
-import ForgotPassword from "./pages/ForgotPassword";
+import Aquariums from "./pages/dashboard/Aquariums";
+//import Home from "./pages/Home";
+//import Register from "./pages/Register";
+//import Login from "./pages/Login";
+//import Public from "./layout/Public";
+//import ForgotPassword from "./pages/ForgotPassword";
 
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Public />}>
-          <Route path="" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
-        </Route>
-      </Routes>
+      <Aquariums></Aquariums>
     </>
   )
 }
