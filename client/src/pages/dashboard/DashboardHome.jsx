c8d8fccebd6f52f778eba52b2c7135ce120e8fb0
Sara Spano <saraspano@live.com>
2025-05-13 13:02:53 +0200

    fix file

--- client/src/pages/dashboard/DashboardHome.jsx
+++ client/src/pages/dashboard/DashboardHome.jsx
@@ -29,12 +29,12 @@ const tanks = [
 
 {/* Storico GH/KH per ciascun acquario*/}
 const history = {
-  "Tropical Paradise": [
+  "Tank 1": [
     { date: '2025-01-01', gh: 7, kh: 5 },
     { date: '2025-02-01', gh: 8, kh: 6 },
     { date: '2025-03-01', gh: 6, kh: 5 }
   ],
-  "Amazon Basin": [
+  "Tank 2": [
     { date: '2025-01-01', gh: 4, kh: 3 },
     { date: '2025-02-01', gh: 5, kh: 3 },
     { date: '2025-03-01', gh: 4, kh: 2 }
