51c64d2abc7391e0fb3d40a9b66ba73b40ee1c28
just-silvia <barbagallo.silvia02@gmail.com>
2025-05-19 17:35:42 +0200

    added api for the registration and logo that makes u go back to homepage

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CustomButton from "../components/shared/CustomButton";
 import img_register from "../assets/img_form_register.JPG";
 import { validatePassword } from "../utilities/secure";
 import RevealInput from "../components/shared/RevealInput";
 import RevealValidatePassword from "../components/shared/RevealValidatePassword";
+import { useApi } from "../hooks/useApi";
+import { toast } from "react-toastify";
+import logo from "../assets/logo-sidebar/logo_sidebar.png"
 
 const Register = () => {
     const [form, setForm] = useState({
@@ -12,20 +15,20 @@ const Register = () => {
         lastName: "",
         email: "",
         password: "",
-        confirmPassword: "",
-        subscribeNews: false
+        confirmPassword: ""
     });
 
-    const [error, setError] = useState(false);
+    const { post } = useApi();
+    const navigate = useNavigate();
+
+    const [error, setError] = useState(null);
 
     const handleChange = (e) => {
-        const { name, value, type, checked } = e.target;
+        const { name, value } = e.target;
 
-        const newValue = type === 'checkbox' ? checked : value;
-
-        setForm((_form) => ({
-            ..._form,
-            [name]: newValue
+        setForm((prevForm) => ({
+            ...prevForm,
+            [name]: value
         }));
     };
 
@@ -35,7 +38,7 @@ const Register = () => {
         }
     }, [form.password, form.confirmPassword]);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         if (error) {
@@ -43,20 +46,42 @@ const Register = () => {
         }
 
         if (form.password !== form.confirmPassword) {
-            setError("Le password non coincidono.");
+            setError("Passwords do not match.");
+            return;
+        }
+
+        const passwordErrors = validatePassword(form.password);
+        if (passwordErrors.length > 0) {
+            setError("Password does not meet the required criteria.");
             return;
         }
 
         console.log("Dati registrazione:", form);
+
+        const { firstName, lastName, email, password } = form;
+        try {
+            await post("/register", { firstName, lastName, email, password }, "API");
+
+            toast.success("Register done successfully!");
+            navigate("/app");
+        } catch (error) {
+            console.log(error);
+            toast.error("Error during registration, try again!");
+        }
     };
 
     return (
-        <div className="min-h-screen grid grid-cols-1 md:grid-cols-2">
-            <div className="flex items-center justify-center bg-light dark:bg-dark px-4 py-10 sm:px-6 lg:px-8">
+        <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 dark:bg-gray-900 dark:text-white"> 
+            <div className="absolute top-6 left-6">
+                <Link to="/">
+                    <img src={logo} alt="Logo" className="h-10 w-auto" />
+                </Link>
+            </div>
+            <div className="flex items-center justify-center bg-light px-4 py-10 sm:px-6 lg:px-8">
                 <form onSubmit={handleSubmit} className="w-full max-w-md space-y-6">
                     <h2 className="text-center">Register</h2>
                     <div>
-                        <label className="block font-medium mb-1 text-dark dark:text-gray-200">
+                        <label className="block font-medium mb-1">
                             First Name
                             <span className="!text-red-500">*</span>
                         </label>
@@ -67,11 +92,11 @@ const Register = () => {
                             value={form.firstName}
                             onChange={handleChange}
                             required
-                            className="w-full p-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent dark:bg-gray-700 dark:text-white"
+                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent"
                         />
                     </div>
                     <div>
-                        <label className="block font-medium mb-1 text-dark dark:text-gray-200">
+                        <label className="block font-medium mb-1">
                             Last Name
                             <span className="!text-red-500">*</span>
                         </label>
@@ -82,11 +107,11 @@ const Register = () => {
                             value={form.lastName}
                             onChange={handleChange}
                             required
-                            className="w-full p-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent dark:bg-gray-700 dark:text-white"
+                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent"
                         />
                     </div>
                     <div>
-                        <label className="block font-medium mb-1 text-dark dark:text-gray-200">
+                        <label className="block font-medium mb-1">
                             Email
                             <span className="!text-red-500">*</span>
                         </label>
@@ -97,11 +122,11 @@ const Register = () => {
                             value={form.email}
                             onChange={handleChange}
                             required
-                            className="w-full p-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent dark:bg-gray-700 dark:text-white"
+                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent"
                         />
                     </div>
                     <div>
-                        <label className="block font-medium mb-1 text-dark dark:text-gray-200">
+                        <label className="block font-medium mb-1">
                             Password
                             <span className="!text-red-500">*</span>
                         </label>
@@ -111,34 +136,34 @@ const Register = () => {
                             value={form.password}
                             onInput={handleChange}
                             required
-                            inputClassName="w-full p-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent dark:bg-gray-700 dark:text-white"
+                            inputClassName="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent"
                             errorsClassName="flex flex-col gap-1 mt-1"
                         />
                     </div>
                     <div>
-                        <label className="block font-medium mb-1 text-dark dark:text-gray-200">
-                            Conferma Password <span className="!text-red-500">*</span>
+                        <label className="block font-medium mb-1">
+                            Confirm Password <span className="!text-red-500">*</span>
                         </label>
                         <RevealInput
                             name="confirmPassword"
                             value={form.confirmPassword}
                             onInput={handleChange}
                             required
-                            placeholder="Conferma Password"
-                            className="w-full p-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent dark:bg-gray-700 dark:text-white"
+                            placeholder="Confirm Password"
+                            className="w-full p-3 border border-gray-300 focus:outline-none rounded-lg focus:ring-2 focus:ring-accent"
                         />
                         {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
                     </div>
                     <div className="flex flex-col sm:flex-row justify-between items-center gap-4 mt-4">
-                        <p className="text-sm text-dark dark:text-gray-200">
-                            Have an account? <Link to="/login" className="text-accent dark:text-accent hover:text-primary dark:hover:text-gray-300 transition-colors">Login</Link>
+                        <p className="text-sm">
+                            Have an account? <Link to="/login" className="text-accent hover:text-primary transition-colors">Login</Link>
                         </p>
                         <CustomButton
                             type="submit" className="px-6 py-2">Register</CustomButton>
                     </div>
                 </form>
             </div>
-            <div className="hidden md:flex h-screen items-center justify-center bg-gray-100 dark:bg-gray-800">
+            <div className="hidden md:flex items-center justify-center bg-gray-100">
                 <img
                     src={img_register}
                     alt="Acquario register"
