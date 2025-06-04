a6de59451d8345f991a13c45f220f29cbd68e599
Sara Spano <saraspano@live.com>
2025-06-04 19:17:47 +0200

    fixed vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: 'http://192.168.1.20:3000',
+        target: 'http://192.168.249.49:3000',
         changeOrigin: true,
       },
       "/auth": {
-        target: 'http://192.168.1.20:3000',
+        target: 'http://192.168.249.49:3000',
         changeOrigin: true,
       }
     }
